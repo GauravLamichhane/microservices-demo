b18c7fc0afe2ec633a7fc295e77ca011aa2a7351
Gaurav <gaurav.lamichhane02@gmail.com>
2026-07-30 23:43:29 +0545

    add logs to the taks in the main.py

--- main/tasks.py
+++ main/tasks.py
@@ -6,20 +6,21 @@ from producer import publish
 @shared_task
 def publish_outbox_events():
     with app.app_context():
-        events = PublishedEvent.query.filter_by(
-            is_consumed=False
-        ).all()
+        events = PublishedEvent.query.filter_by(is_consumed=False).all()
+
+        print(f"Found {len(events)} unpublished events")
 
         for event in events:
+            print(f"Publishing event {event.id}")
+
             try:
-                publish(
-                    event.extra["type"],
-                    event.payload,
-                )
+                publish(event.extra["type"], event.payload)
 
                 event.is_consumed = True
+                print(f"Published event {event.id}")
 
             except Exception as e:
-                print(f"Failed to publish event {event.id}: {e}")
+                print(f"Failed event {event.id}: {e}")
 
         db.session.commit()
+        print("Commit complete")
