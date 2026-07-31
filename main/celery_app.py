c687cf72111d07897b0d862a150e3d99d7d20a51
Gaurav <gaurav.lamichhane02@gmail.com>
2026-07-31 14:46:10 +0545

    fix: concurrency to solo pool in celery

--- main/celery_app.py
+++ main/celery_app.py
@@ -1,4 +1,5 @@
 from celery import Celery
+from celery.signals import worker_process_init
 from main import app
 
 celery = Celery(
@@ -6,8 +7,12 @@ celery = Celery(
     broker=app.config["CELERY_BROKER_URL"],
     backend=app.config["CELERY_RESULT_BACKEND"],
 )
-
 import tasks
+
+@worker_process_init.connect
+def init_worker(**kwargs):
+    from main import db
+    db.engine.dispose()
 
 celery.conf.beat_schedule = {
     "publish-outbox-events": {
